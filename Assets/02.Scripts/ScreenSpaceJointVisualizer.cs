fd2da273f8dfaad5ad5568a3996b9dc95fcf9863
jhuh3226 <27257844+jhuh3226@users.noreply.github.com>
2022-01-30 14:35:33 -0500

    Added right leg flamingo posture

--- Assets/02.Scripts/ScreenSpaceJointVisualizer.cs
+++ Assets/02.Scripts/ScreenSpaceJointVisualizer.cs
@@ -1,18 +1,19 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
+using System.Text;
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-namespace UnityEngine.XR.ARFoundation.Samples
-{
-    public class ScreenSpaceJointVisualizer : MonoBehaviour
-    {
+namespace UnityEngine.XR.ARFoundation.Samples {
+    public class ScreenSpaceJointVisualizer : MonoBehaviour {
+        // added
+        public GameObject UpRightSphere, RightSphere, RightFootSphere, cvFlamingoCheck;
+        float tiltA, tiltB, tiltC = 0;
+
         // 2D joint skeleton
-        enum JointIndices
-        {
+        enum JointIndices {
             Invalid = -1,
             Head = 0, // parent: Neck1 [1]
             Neck1 = 1, // parent: Root [16]
@@ -34,79 +35,87 @@ namespace UnityEngine.XR.ARFoundation.Samples
         }
 
         [SerializeField]
-        [Tooltip("The AR camera being used in the scene.")]
+        [Tooltip ("The AR camera being used in the scene.")]
         Camera m_ARCamera;
 
         /// <summary>
         /// Get or set the <c>Camera</c>.
         /// </summary>
-        public Camera arCamera
-        {
+        public Camera arCamera {
             get { return m_ARCamera; }
             set { m_ARCamera = value; }
         }
 
         [SerializeField]
-        [Tooltip("The ARHumanBodyManager which will produce human body anchors.")]
+        [Tooltip ("The ARHumanBodyManager which will produce human body anchors.")]
         ARHumanBodyManager m_HumanBodyManager;
 
         /// <summary>
         /// Get or set the <c>ARHumanBodyManager</c>.
         /// </summary>
-        public ARHumanBodyManager humanBodyManager
-        {
+        public ARHumanBodyManager humanBodyManager {
             get { return m_HumanBodyManager; }
             set { m_HumanBodyManager = value; }
         }
 
         [SerializeField]
-        [Tooltip("A prefab that contains a LineRenderer component that will be used for rendering lines, representing the skeleton joints.")]
+        [Tooltip ("A prefab that contains a LineRenderer component that will be used for rendering lines, representing the skeleton joints.")]
         GameObject m_LineRendererPrefab;
 
         /// <summary>
         /// Get or set the Line Renderer prefab.
         /// </summary>
-        public GameObject lineRendererPrefab
-        {
+        public GameObject lineRendererPrefab {
             get { return m_LineRendererPrefab; }
             set { m_LineRendererPrefab = value; }
         }
 
         Dictionary<int, GameObject> m_LineRenderers;
-        static HashSet<int> s_JointSet = new HashSet<int>();
+        static HashSet<int> s_JointSet = new HashSet<int> ();
 
-        void Awake()
-        {
-            m_LineRenderers = new Dictionary<int, GameObject>();
+        void Awake () {
+            m_LineRenderers = new Dictionary<int, GameObject> ();
         }
 
-        void UpdateRenderer(NativeArray<XRHumanBodyPose2DJoint> joints, int index)
-        {
+        void UpdateRenderer (NativeArray<XRHumanBodyPose2DJoint> joints, int index) {
             GameObject lineRendererGO;
-            if (!m_LineRenderers.TryGetValue(index, out lineRendererGO))
-            {
-                lineRendererGO = Instantiate(m_LineRendererPrefab, transform);
-                m_LineRenderers.Add(index, lineRendererGO);
+            if (!m_LineRenderers.TryGetValue (index, out lineRendererGO)) {
+                lineRendererGO = Instantiate (m_LineRendererPrefab, transform);
+                m_LineRenderers.Add (index, lineRendererGO);
             }
 
-            var lineRenderer = lineRendererGO.GetComponent<LineRenderer>();
+            var lineRenderer = lineRendererGO.GetComponent<LineRenderer> ();
 
             // Traverse hierarchy to determine the longest line set that needs to be drawn.
-            var positions = new NativeArray<Vector2>(joints.Length, Allocator.Temp);
-            try
-            {
+            var positions = new NativeArray<Vector2> (joints.Length, Allocator.Temp);
+            try {
                 var boneIndex = index;
                 int jointCount = 0;
-                while (boneIndex >= 0)
-                {
+                while (boneIndex >= 0) {
                     var joint = joints[boneIndex];
-                    if (joint.tracked)
-                    {
-                        positions[jointCount++] = joint.position;
-                        if (!s_JointSet.Add(boneIndex))
+                    if (joint.tracked) {
+                        // positions[jointCount++] = joint.position;
+
+                        //added
+                        // Debug.Log ("upRightLeg" + joints[8].position + "rightLeg: " + joints[9].position);
+                        UpRightSphere.transform.position = joints[8].position;
+                        RightSphere.transform.position = joints[9].position;
+                        RightFootSphere.transform.position = joints[10].position;
+                        tiltA = (joints[8].position.y - joints[9].position.y) / (joints[8].position.x - joints[9].position.x);
+                        tiltB = (joints[9].position.y - joints[10].position.y) / (joints[9].position.x - joints[10].position.x);
+                        tiltC = (joints[8].position.y - joints[10].position.y) / (joints[8].position.x - joints[10].position.x);
+
+                        if ((tiltA * 100 > 15 && tiltA * 100 < 45) && (tiltB * 100 > -25 && tiltB * 100 < 0)) {
+                            // Debug.Log ("flamingo posture");
+                            cvFlamingoCheck.SetActive (true);
+                        } else {
+                            cvFlamingoCheck.SetActive (false);
+                            Debug.Log ("tiltA: " + tiltA * 100 + ", tiltB: " + tiltB * 100 + ", tiltC: " + tiltC * 100);
+                        }
+
+                        if (!s_JointSet.Add (boneIndex))
                             break;
-                    }
-                    else
+                    } else
                         break;
 
                     boneIndex = joint.parentIndex;
@@ -116,47 +125,38 @@ namespace UnityEngine.XR.ARFoundation.Samples
                 lineRenderer.positionCount = jointCount;
                 lineRenderer.startWidth = 0.001f;
                 lineRenderer.endWidth = 0.001f;
-                for (int i = 0; i < jointCount; ++i)
-                {
+                for (int i = 0; i < jointCount; ++i) {
                     var position = positions[i];
-                    var worldPosition = m_ARCamera.ViewportToWorldPoint(
-                        new Vector3(position.x, position.y, m_ARCamera.nearClipPlane));
-                    lineRenderer.SetPosition(i, worldPosition);
+                    var worldPosition = m_ARCamera.ViewportToWorldPoint (
+                        new Vector3 (position.x, position.y, m_ARCamera.nearClipPlane));
+                    lineRenderer.SetPosition (i, worldPosition);
                 }
-                lineRendererGO.SetActive(true);
-            }
-            finally
-            {
-                positions.Dispose();
+                lineRendererGO.SetActive (true);
+            } finally {
+                positions.Dispose ();
             }
         }
 
-        void Update()
-        {
-            Debug.Assert(m_HumanBodyManager != null, "Human body manager cannot be null");
-            var joints = m_HumanBodyManager.GetHumanBodyPose2DJoints(Allocator.Temp);
-            if (!joints.IsCreated)
-            {
-                HideJointLines();
+        void Update () {
+            Debug.Assert (m_HumanBodyManager != null, "Human body manager cannot be null");
+            var joints = m_HumanBodyManager.GetHumanBodyPose2DJoints (Allocator.Temp);
+            if (!joints.IsCreated) {
+                HideJointLines ();
                 return;
             }
 
-            using (joints)
-            {
-                s_JointSet.Clear();
-                for (int i = joints.Length - 1; i >= 0; --i)
-                {
+            using (joints) {
+                s_JointSet.Clear ();
+                for (int i = joints.Length - 1; i >= 0; --i) {
                     if (joints[i].parentIndex != -1)
-                        UpdateRenderer(joints, i);
+                        UpdateRenderer (joints, i);
                 }
             }
         }
 
-        void HideJointLines()
-        {
-            foreach (var lineRenderer in m_LineRenderers)
-            {
-                lineRenderer.Value.SetActive(false);
+        void HideJointLines () {
+            foreach (var lineRenderer in m_LineRenderers) {
+                lineRenderer.Value.SetActive (false);
             }
         }
     }
